a8112abde878101642835cd5416f706ede57d27f
Gandhi20 <gandhi20.rblx@gmail.com>
2024-08-28 13:57:07 -0400

    v1

---
+++ index.js
@@ -0,0 +1,18 @@
+// Database
+const { initializeApp, cert } = require('firebase-admin/app');
+const firebase_Account = require("./firebase.json");
+
+initializeApp({
+  credential: cert(firebase_Account)
+});
+
+// Server
+const express = require('express');
+
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+
+app.use("/", require("./api/fetch"));
+
+app.listen(777, () => console.log(`:: SideTech :: Verification system is online!`));
